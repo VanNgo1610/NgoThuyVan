27565c05aa23a52b4c9237bfba35968d56702c30
VanNgo1610 <vanthngo.csp@gmail.com>
2023-09-06 11:12:25 +0700

    Create lenh atm rut tien.c

---
+++ sem1/lenh atm rut tien.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+    int tknh;
+    char mktk;
+    tknh=1000000;
+    printf("Chuong trinh may rut tien ATM");
+    printf("Nhap so tien can rut");
+    scanf("%d",&tien);
+    sodu -= tien
+    printf("So tien trong tai khoan cua quy khach la: %d",sodu);
+    if(sodu>=tien){
+        printf("So tien con lai trong tai khoan cua quy khach la:%d",sodu);
+        if(tien%50000!=0){
+            print("So tien can rut la boi so cua 50000")}
+        }
+    else{
+        printf("So tien trong tai khoan cua quy khach khong du de thuc hien giao dich")
+    }
+    }
