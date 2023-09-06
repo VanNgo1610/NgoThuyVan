682107e37e48ef89f057a5f79f2e461355313376
VanNgo1610 <vanthngo.csp@gmail.com>
2023-09-06 11:35:51 +0700

    Create try it yourself 7.2.c

---
+++ sem1/session 7+8/bài 7/try it yourself 7.2.c
@@ -0,0 +1,30 @@
+//Write a program to accept 2 numbers and tell whether the product of the two numbers is equal to or greater than 1000.
+#include <stdio.h>
+int main(){
+    printf( "Input two integer separated by whitespace: "); 
+    int a; 
+    int b; 
+    scanf("%d %d", &a, &b);
+    int productcode;
+    float orderamount; 
+    printf(“\n Please enter the product code :” );
+    scanf(“%d”, &productcode);
+    printf(“Please enter the order amount :” );
+    scanf(“ %f” , &orderamount);
+    if(productcode == a){
+        if(orderamount >=1000){
+            printf("Greater")
+        }
+        else{
+            printf("Less")
+        }
+    }
+    if(productcode == b){
+        if(orderamount >=1000){
+            printf("Greater")
+        }
+        else{
+            printf("Less")
+        }
+    }
+}
