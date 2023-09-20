01b7a00c3554f653671585a48e9b02f9b818e7eb
VanNgo1610 <vanthngo.csp@gmail.com>
2023-09-20 08:43:34 +0700

    Update tinh chu vi va dien tich HCN.c

--- sem1/tinh chu vi va dien tich HCN/tinh chu vi va dien tich HCN.c
+++ sem1/tinh chu vi va dien tich HCN/tinh chu vi va dien tich HCN.c
@@ -1,25 +1,28 @@
 #include<stdio.h>
 
-void main(){
+int ChuViHCN(int a , int b);
+int DienTichHCN(int a , int b);
+int main(){
 	int a, b;
 	printf("Nhap so do 2 canh hinh chu nhat: ");
 	printf("\nNhap so do chieu dai: ");
 	scanf("%d", &a);
 	printf("\nNhap so do chieu rong: ");
 	scanf("%d",&b);
-	ChuViHCN(a, b);
-    DienTichHCN(a, b);
+	
+	printf("\nChu vi hinh chu nhat la: %d", ChuViHCN(a,b));
+	printf("\nDien tich hinh chu nhat la: %d", DienTichHCN(a,b));
+    
+    return 0;
 }
 
-void ChuViHCN(int a, int b){
-	int P;
-	P = (a+b) * 2;
-	printf("\nChu vi hinh chu nhat la: %d", P);
+int ChuViHCN(int a, int b){
+	
+	return (a+b)*2;
 }
 
-void DienTichHCN(int a, int b){
-	int S;
-	S = a * b;
-	printf("\nDien tich hinh chu nhat la: %d", S);
+int DienTichHCN(int a, int b){
+	
+	return a*b;
 }
 
